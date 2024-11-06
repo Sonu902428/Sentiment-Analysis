033b6adc3e8855eec8b80352e0dd8cc1b49886a4
Sonu Kumawat <125640335+Sonu902428@users.noreply.github.com>
2024-11-06 23:33:32 +0530

    Update myapi.py
    use the appropriate api to replace with API

--- myapi.py
+++ myapi.py
@@ -3,7 +3,7 @@ import paralleldots
 # Setting your API key
 class API:
     def __init__(self):
-        paralleldots.set_api_key('baLnjigzhEcLZdq8tmbQpaLPfB1Jrn2HmspltfOxnxI')
+        paralleldots.set_api_key('API')
     def sentiment_analysis(self,text):
         response=paralleldots.sentiment(text)
         return response
